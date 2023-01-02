e37994490336ca652a7c9ef7dece845fecaaa76c
tobiitum <tbs.kling@gmail.com>
2023-01-02 17:18:49 +0100

    Initial Commit

---
+++ telo/teloApp.swift
@@ -0,0 +1,17 @@
+//
+//  teloApp.swift
+//  telo
+//
+//  Created by Tobias Klingenberg on 02.01.23.
+//
+
+import SwiftUI
+
+@main
+struct teloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
